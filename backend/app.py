487dc22a7908ccd76667012df5989d074439704a
Pawciuz <podgorskipawel16@gmail.com>
2024-08-20 23:37:54 +0200

    make it better

--- backend/app.py
+++ backend/app.py
@@ -6,13 +6,15 @@ import pytz
 import os
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
+app = Flask(__name__, static_folder='../frontend/build')
 CORS(app)
 
 @app.route('/')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
-
+@app.route('/<path:path>')
+def static_proxy(path):
+    return send_from_directory(app.static_folder, path)
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
