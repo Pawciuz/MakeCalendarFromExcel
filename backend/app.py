35d9f90d7afa6c2513a2b02b823a61af36986765
Paweł Podgórski <podgorskipawel16@gmail.com>
2024-09-25 00:01:13 +0200

    make it better

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,14 @@ from flask import Flask, request, send_file, send_from_directory
 import pandas as pd
 from ics import Calendar, Event
 from datetime import datetime
+
+from ics.grammar.parse import ContentLine
+from pandas.io.parsers.base_parser import DatetimeIndex
 import pytz
 import os
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
+app = Flask(__name__, static_folder="../frontend/build", static_url_path="")
 CORS(app)
 
 
@@ -15,46 +18,70 @@ def remove_previous_calendar(ics_file):
         os.remove(ics_file)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return send_from_directory(app.static_folder, 'index.html')
+    if app.static_folder:
+        return send_from_directory(app.static_folder, "index.html")
+    else:
+        return "Static folder not found", 500
 
 
-@app.route('/upload', methods=['POST'])
+@app.route("/upload", methods=["POST"])
 def upload_file():
-    if 'file' not in request.files:
+    if "file" not in request.files:
         return "Brak pliku", 400
 
-    file = request.files['file']
-    if file.filename == '':
+    file = request.files["file"]
+    if file.filename == "":
         return "Brak pliku", 400
 
-    if file and file.filename.endswith(('.xlsx', '.xls')):
+    if file and file.filename and file.filename.endswith((".xlsx", ".xls")):
         try:
-            df = pd.read_excel(file)
+            df = pd.read_excel(
+                file,
+            )
 
             calendar = Calendar()
-            poland_tz = pytz.timezone('Europe/Warsaw')
+            poland_tz = pytz.timezone("Europe/Warsaw")
 
             for index, row in df.iterrows():
-                if pd.notna(row['Godzina rozpoczecia']) and pd.notna(row['Godzina zakonczenia']):
+                if bool(pd.notna(row["Godzina rozpoczecia"])) and bool(
+                    pd.notna(row["Godzina zakonczenia"])
+                ):
                     event = Event()
-                    event.name = row['Nazwa wydarzenia']
-                    start_dt = datetime.combine(row['Data'], row['Godzina rozpoczecia'])
+                    event.name = str(row["Nazwa wydarzenia"])
+                    start_dt = datetime.combine(
+                        row["Data"],
+                        row["Godzina rozpoczecia"],
+                    )
                     start_dt = poland_tz.localize(start_dt)
-                    end_dt = datetime.combine(row['Data'], row['Godzina zakonczenia'])
+                    end_dt = datetime.combine(
+                        row["Data"],
+                        row["Godzina zakonczenia"],
+                    )
                     end_dt = poland_tz.localize(end_dt)
                     event.begin = start_dt
                     event.end = end_dt
-                    calendar.events.add(event)
 
-            ics_file = '/tmp/kalendarz.ics'
+                    if "Powtarzaj" in df.columns and pd.notna(row["Powtarzaj"]):
+                        repeat_interval = str(row["Powtarzaj"]).strip().lower()
+
+                        if repeat_interval == "dzień":
+                            event.extra.append(ContentLine.parse("RRULE:FREQ=DAILY"))
+                        elif repeat_interval == "tydzień":
+                            event.extra.append(ContentLine.parse("RRULE:FREQ=WEEKLY"))
+                        elif repeat_interval == "2 tydzień":
+                            event.extra.append(ContentLine.parse("RRULE:FREQ=WEEKLY;INTERVAL=2"))
+                        elif repeat_interval == "miesiąc":
+                            event.extra.append(ContentLine.parse("RRULE:FREQ=MONTHLY"))
+                    calendar.events.add(event)
+            ics_file = "/tmp/kalendarz.ics"
 
             remove_previous_calendar(ics_file)
 
-            with open(ics_file, 'w') as f:
+            with open(ics_file, "w") as f:
                 f.writelines(calendar)
-                print(f'Plik {ics_file} został zapisany.')
+                print(f"Plik {ics_file} został zapisany.")
 
             return send_file(ics_file, as_attachment=True)
 
@@ -65,4 +92,4 @@ def upload_file():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5050)),debug=True)
--- frontend/src/components/SampleTable.js
+++ frontend/src/components/SampleTable.js
@@ -4,32 +4,36 @@ const SampleTable = () => {
     return (
         <table>
             <thead>
-                <tr>
-                    <th>Data</th>
-                    <th>Godzina rozpoczecia</th>
-                    <th>Godzina zakonczenia</th>
-                    <th>Nazwa wydarzenia</th>
-                </tr>
+            <tr>
+                <th>Data</th>
+                <th>Godzina rozpoczecia</th>
+                <th>Godzina zakonczenia</th>
+                <th>Nazwa wydarzenia</th>
+                <th>Powtarzaj (optional)</th>
+            </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>2024-08-01</td>
-                    <td>10:00:00</td>
-                    <td>17:00:00</td>
-                    <td>Spotkanie z zespołem</td>
+            <tr>
+                <td>2024-08-01</td>
+                <td>10:00:00</td>
+                <td>17:00:00</td>
+                <td>Spotkanie z zespołem</td>
+                <td>tydzień</td>
+            </tr>
+            <tr>
+                <td>2024-08-02</td>
+                <td>12:00:00</td>
+                <td>13:00:00</td>
+                <td>Lunch z klientem</td>
+                <td>2 tydzień</td>
                 </tr>
-                <tr>
-                    <td>2024-08-02</td>
-                    <td>12:00:00</td>
-                    <td>13:00:00</td>
-                    <td>Lunch z klientem</td>
-                </tr>
-                <tr>
-                    <td>2024-08-03</td>
-                    <td>09:00:00</td>
-                    <td>11:00:00</td>
-                    <td>Prezentacja projektu</td>
-                </tr>
+            <tr>
+                <td>2024-08-03</td>
+                <td>09:00:00</td>
+                <td>11:00:00</td>
+                <td>Prezentacja projektu</td>
+                <td>miesiąc</td>
+            </tr>
             </tbody>
         </table>
     );
